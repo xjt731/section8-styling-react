58c1269521ebc768e46b09b8436f6e150e358801
gilbert777 <xjt674471772@mail.com>
2023-03-25 19:05:49 +1100

    styling option item

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Option = (props) => (
-  <div>
-    {props.optionText}
+  <div className="option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
     <button
-      className='button button--link'
+      className="button button--link"
       onClick={(e) => {
         props.handleDeleteOption(props.optionText);
       }}
     >
       remove
-    </button>
+      </button>
   </div>
 );
 
