3ad727aad2ad126ec4d811d2249943ba30b8424c
gilbert777 <xjt674471772@mail.com>
2023-03-27 19:59:56 +1100

    build it: router for portfolio site

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <h1>Portfolio</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/portfolio" activeClassName="is-active" exact={true}>Portfolio</NavLink>
+    <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
   </header>
 );
 
