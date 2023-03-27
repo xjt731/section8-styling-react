72e39e547ac6ed472494b7c71a4bd76a105a9c74
gilbert777 <xjt674471772@mail.com>
2023-03-27 20:27:42 +1100

    subscribing and destructiong

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
       };
     case 'RESET':
       return {
@@ -19,13 +25,13 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
-// Actions - than an object that gets sent to the store
-
-// I'd like to increment the count
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
 
 store.dispatch({
@@ -40,7 +46,12 @@ store.dispatch({
   type: 'DECREMENT'
 });
 
-// I'd like to reset the count to zero
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
 
-
-console.log(store.getState());
+store.dispatch({
+  type: 'SET',
+  count: 101
+});
